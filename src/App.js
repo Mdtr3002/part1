532a113e7e0855c538d64bde3fc06a40f35e8711
Trần Duy Minh <minhduytranct2017@gmail.com>
2021-11-13 21:00:56 +0700

    Exercises 1.12 to 1.14 submission

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
-
-const Header = (headerprop) => {
-  return(
-    <h1>{headerprop.course}</h1>
-  )
-  }
-  
-  const Part = (partprop) => {
-    return (
-      <p>{partprop.part} {partprop.exercises}</p>
-    )
-  }
-  const Content = (contentprop) => {
+import React, { useState } from 'react'
+const Button = (props) =>
+<button onClick={props.func}>{props.text}</button>
+const ShowAncedote = ({anecdotes,vote}) => {
   return(
     <>
-    <Part part={contentprop.parts[0].name} exercises={contentprop.parts[0].exercises} />
-    <Part part={contentprop.parts[1].name} exercises={contentprop.parts[1].exercises} />
-    <Part part={contentprop.parts[2].name} exercises={contentprop.parts[2].exercises} />
+    {anecdotes}<br/>
+      has {vote} votes<br/>
     </>
   )
-  }
-  
-  const Footer = (footerProp) => {
-    return(
-     <p>Number of exercises {footerProp.parts[0].exercises + footerProp.parts[1].exercises + footerProp.parts[2].exercises}</p>
-    )
-  }
-  const App = () => {
-    const course = {
-      name: 'Half Stack application development',
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7
-        },
-        {
-          name: 'State of a component',
-          exercises: 14
-        }
-      ]
+}
+const Max = ({number, anecdotes}) => {
+  let max = number[0] 
+  let mostVoted = 0
+  for(let i=0;i<7;i++) {
+    if (max<number[i]) {
+      max = number[i]
+      mostVoted = i
     }
-  
-  
-  
-    return (
-      <div>
-        <Header course={course.name}/>
-        <Content parts={course.parts}/>
-        <Footer  parts={course.parts} />
-      </div>
-    )
   }
+  return (
+    <div>
+      <h1>Anecdote with most votes</h1>
+      <ShowAncedote anecdotes={anecdotes[mostVoted]} vote={number[mostVoted]} />
+    </div>
+  )
+}
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+  const [vote, setVote] = useState({0:0,1:0,2:0,3:0,4:0,5:0,6:0})
+  const [selected, setSelected] = useState(0)
+  const minh = {...vote}
+  const countVote = () => {
+    minh[selected]+=1
+    setVote({...minh})
+  }
+  const random = () => 
+  setSelected(Math.floor(Math.random()*7))
+  console.log(vote[selected])
+  return (
+    <div>
+      <ShowAncedote anecdotes={anecdotes[selected]} vote={vote[selected]} />
+      <Button func={() => countVote()} text={"vote"} />
+      <Button func={() => random()} text={"next ancedote"} />
+      <Max anecdotes={anecdotes} number={vote} />
+    </div>
+  )
+}
 export default App;
